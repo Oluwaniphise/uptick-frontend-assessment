e0441356fff77ee45f4baf5f85b6dc82a915f276
Oduyale Enoch <oduyaleenoch@gmail.com>
2023-02-21 16:11:34 -0800

    completed all tasks

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,11 @@
-import React from 'react'
+import React from "react";
 
 export const Navbar = () => {
   return (
+    <nav className="container flex justify-between items-center mx-auto pt-[1rem]">
+      <h1 className="font-bold text-[2rem] ">Uptick.</h1>
 
-    <nav className='container flex justify-between items-center mx-auto pt-[1rem]'>
-        <h1 className='font-bold text-[2rem] '>Uptick.</h1>
-
-
-        <h1>Menu.</h1>
+      <h1>Menu.</h1>
     </nav>
-  )
-}
+  );
+};
